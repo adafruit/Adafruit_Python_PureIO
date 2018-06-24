c8b069d83d15d85e16504606277b2453b2754548
ladyada <limor@ladyada.net>
2018-06-24 17:26:23 -0400

    fix line

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: System :: Hardware']
 
 setup(name              = 'Adafruit_PureIO',
-      version           = '0.2.2,
+      version           = '0.2.2',
+	  url               = 'https://github.com/adafruit/Adafruit_Python_PureIO',
       author            = 'Tony DiCola / Adafruit Industries',
       author_email      = 'support@adafruit.com',
       description       = 'Pure python (i.e. no native extensions) access to Linux IO including I2C and SPI.  Drop in replacement for smbus and spidev modules.',
